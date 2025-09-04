ba60b8a3279315edc363d94d5f216a275a227f38
nolang22 <rnrmfwwkd22@gmail.com>
2025-09-04 15:28:38 +0900

    2025 09 04

---
+++ src/main/java/org/example/o_lim/OLimApplication.java
@@ -0,0 +1,13 @@
+package org.example.o_lim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OLimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OLimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/o_lim/OLimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.o_lim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OLimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
