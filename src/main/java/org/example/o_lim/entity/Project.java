19d96337b1d57bd83ff6ec486a846b66adb7e449
rpsnrpsn <rpsnrpsn@naver.com>
2025-09-08 17:51:19 +0900

    20250908 프로젝트

--- src/main/java/org/example/o_lim/entity/Project.java
+++ src/main/java/org/example/o_lim/entity/Project.java
@@ -1,4 +1,40 @@
 package org.example.o_lim.entity;
 
-public class Project {
+import org.example.o_lim.entity.base.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "projects"
+
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter
+public class Project extends BaseTimeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, updatable = false)
+  private Long id;
+
+  @Column(name = "owner_id", updatable = false, nullable = false)
+  private Long owner_id;
+
+  @Column(name = "title", nullable = false)
+  private String title;
+
+  @Column(name = "content", nullable = false)
+  private String content;
+
 }
