1e65ca7bcd7e33446f5bd2d7f79217e06e8e8d33
nolang22 <rnrmfwwkd22@gmail.com>
2025-09-09 18:19:25 +0900

    2025 09 09

--- src/main/java/org/example/o_lim/controller/TagController.java
+++ src/main/java/org/example/o_lim/controller/TagController.java
@@ -1,12 +1,23 @@
 package org.example.o_lim.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.o_lim.common.constants.ApiMappingPattern;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 // "/api/v1/projects"
-@RequestMapping()
+@RequestMapping(ApiMappingPattern.Tags.ROOT)
 public class TagController {
+
+    // 생성 "/api/v1/projects/{projectId}/tags"
+     @PostMapping
+
+
+    // 삭제 "/api/v1/projects/{projectId}/tags/{tagId}"
+    @DeleteMapping(ApiMappingPattern.Tags.BY_ID)
+
 }
