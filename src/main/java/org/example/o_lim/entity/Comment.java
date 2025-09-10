e56c67d6ac579c5ada297ff39371e6ec9d34163c
nolang22 <rnrmfwwkd22@gmail.com>
2025-09-10 17:35:02 +0900

    2025 09 10

--- src/main/java/org/example/o_lim/entity/Comment.java
+++ src/main/java/org/example/o_lim/entity/Comment.java
@@ -1,17 +1,28 @@
 package org.example.o_lim.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "comment")
+@Table(name = "comments")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "task_id", nullable = false)
-    private Long authorId;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "task_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_comment_task"))
+    private Task task;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id", nullable = false,
+            foreignKey = @ForeignKey(name = "fk_comment_author"))
+    private User author;
 
     @Column(name = "content", nullable = false)
     private String content;
