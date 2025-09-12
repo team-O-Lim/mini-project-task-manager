8ddc7a490933f831fc768b82e76d3b2d7b3ab0bc
cgung7 <cgung7@naver.com>
2025-09-12 11:58:41 +0900

    0912 feature tasktag entity

--- src/main/java/org/example/o_lim/entity/TaskTag.java
+++ src/main/java/org/example/o_lim/entity/TaskTag.java
@@ -6,10 +6,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "task_tag")
+@Table(name = "task_tag",
+        uniqueConstraints = @UniqueConstraint(name = "uk_task_tag_task_id_tag_id", columnNames = {"task_id", "tag_id"})
+)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TaskTag {
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "task_id",
+            foreignKey = @ForeignKey(name = "fk_task_tag_task"),
+            nullable = false
+    )
+    private Task task;
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "tag_id",
+            foreignKey = @ForeignKey(name = "fk_task_tag_tag")
+    )
+    private Tag tag;
+
 }
+
