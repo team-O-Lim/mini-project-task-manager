0968023bed0d9e4a7b26aca197a3848bb083af80
minion-K <rnjsalsgus06@naver.com>
2025-09-18 11:37:42 +0900

    0918 updated dto/controller

--- src/main/java/org/example/o_lim/security/util/AuthorizationChecker.java
+++ src/main/java/org/example/o_lim/security/util/AuthorizationChecker.java
@@ -1,22 +1,22 @@
-package org.example.o_lim.security.util;
-
-import lombok.RequiredArgsConstructor;
-import org.example.o_lim.entity.TaskAssignees;
-import org.example.o_lim.repository.TaskAssigneesRepository;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
-import java.util.List;
-
-@RequiredArgsConstructor
-@Component("authz")
-public class AuthorizationChecker {
-    private final TaskAssigneesRepository taskAssigneesRepository;
-
-    public boolean isChange(Long taskId, Authentication principal) {
-        if (principal == null || taskId == null) return false;
-
-        String loginId = principal.getName();
-
+//package org.example.o_lim.security.util;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.example.o_lim.entity.TaskAssignees;
+//import org.example.o_lim.repository.TaskAssigneesRepository;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.stereotype.Component;
+//import java.util.List;
+//
+//@RequiredArgsConstructor
+//@Component("authz")
+//public class AuthorizationChecker {
+//    private final TaskAssigneesRepository taskAssigneesRepository;
+//
+//    public boolean isChange(Long taskId, Authentication principal) {
+//        if (principal == null || taskId == null) return false;
+//
+//        String loginId = principal.getName();
+//
 //        List<Boolean> results = taskAssigneesRepository.findByTaskId(taskId).stream()
 //                .map(assignee -> assignee.getAssignees().getLoginId().equals(loginId))
 //                .toList();
@@ -34,5 +34,5 @@ public class AuthorizationChecker {
 //                .map(assignee -> assignee.getAssignees().getLoginId())
 //                .toList();
 //        return assigneeId.contains(loginId);
-    }
-}
+//    }
+//}
