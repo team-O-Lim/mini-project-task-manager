d8fbdcc57fbc722905f4bb3a458d8c48b92026dd
nolang22 <rnrmfwwkd22@gmail.com>
2025-09-18 11:40:13 +0900

    09018 created comment tag serviceimpl/lee

--- src/main/java/org/example/o_lim/repository/TagRepository.java
+++ src/main/java/org/example/o_lim/repository/TagRepository.java
@@ -4,6 +4,9 @@ import org.example.o_lim.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByProjectId(Long projectId);
 }
