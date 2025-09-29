4ce69deee019a8ef755f0aa94d06be47a629b733
Hong-ki-su <gisuhong258@gmail.com>
2025-09-29 16:11:56 +0900

    0929 updated notification responseDto

--- src/main/java/org/example/o_lim/dto/notification/response/NotificationDetailResponseDto.java
+++ src/main/java/org/example/o_lim/dto/notification/response/NotificationDetailResponseDto.java
@@ -1,9 +1,7 @@
 package org.example.o_lim.dto.notification.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.example.o_lim.common.utils.DateUtils;
 import org.example.o_lim.entity.Notification;
-import java.time.LocalDateTime;
 
 public record NotificationDetailResponseDto (
         Long id,
@@ -19,7 +17,6 @@ public record NotificationDetailResponseDto (
                 notification.getContent(),
                 DateUtils.toKstString(notification.getCreatedAt()),
                 DateUtils.toKstString(notification.getUpdatedAt())
-
         );
     }
 }
--- src/main/java/org/example/o_lim/dto/notification/response/NotificationListResponseDto.java
+++ src/main/java/org/example/o_lim/dto/notification/response/NotificationListResponseDto.java
@@ -1,9 +1,7 @@
 package org.example.o_lim.dto.notification.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.example.o_lim.common.utils.DateUtils;
 import org.example.o_lim.entity.Notification;
-import java.time.LocalDateTime;
 
 public record NotificationListResponseDto (
         Long id,
