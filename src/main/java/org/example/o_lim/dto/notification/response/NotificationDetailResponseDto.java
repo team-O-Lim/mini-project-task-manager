06d2d0a31142fc04f8bd6eb8dbd5634cf57fce0f
Hong-ki-su <gisuhong258@gmail.com>
2025-09-29 16:09:25 +0900

    0929 updated notification responseDto

--- src/main/java/org/example/o_lim/dto/notification/response/NotificationDetailResponseDto.java
+++ src/main/java/org/example/o_lim/dto/notification/response/NotificationDetailResponseDto.java
@@ -1,6 +1,7 @@
 package org.example.o_lim.dto.notification.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import org.example.o_lim.common.utils.DateUtils;
 import org.example.o_lim.entity.Notification;
 import java.time.LocalDateTime;
 
@@ -8,18 +9,17 @@ public record NotificationDetailResponseDto (
         Long id,
         String title,
         String content,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-        LocalDateTime createdAt,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-        LocalDateTime updatedAt
+        String createdAt,
+        String updatedAt
 ) {
     public static NotificationDetailResponseDto from (Notification notification) {
         return new NotificationDetailResponseDto(
                 notification.getId(),
                 notification.getTitle(),
                 notification.getContent(),
-                notification.getCreatedAt(),
-                notification.getUpdatedAt()
+                DateUtils.toKstString(notification.getCreatedAt()),
+                DateUtils.toKstString(notification.getUpdatedAt())
+
         );
     }
 }
--- src/main/java/org/example/o_lim/dto/notification/response/NotificationListResponseDto.java
+++ src/main/java/org/example/o_lim/dto/notification/response/NotificationListResponseDto.java
@@ -1,6 +1,7 @@
 package org.example.o_lim.dto.notification.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import org.example.o_lim.common.utils.DateUtils;
 import org.example.o_lim.entity.Notification;
 import java.time.LocalDateTime;
 
@@ -8,15 +9,14 @@ public record NotificationListResponseDto (
         Long id,
         Long project_id,
         String title,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-        LocalDateTime createdAt
+        String createdAt
  ) {
     public static NotificationListResponseDto from(Notification notification) {
       return new NotificationListResponseDto(
               notification.getId(),
               notification.getProject().getId(),
               notification.getTitle(),
-              notification.getCreatedAt()
+              DateUtils.toKstString(notification.getCreatedAt())
       );
     }
 }
