b602975edb538d8290521b8f519644c880536b7e
minion-K <rnjsalsgus06@naver.com>
2025-09-29 15:40:26 +0900

    0929 updated Repository

--- src/main/java/org/example/o_lim/repository/RoleRepository.java
+++ src/main/java/org/example/o_lim/repository/RoleRepository.java
@@ -3,5 +3,7 @@ package org.example.o_lim.repository;
 import org.example.o_lim.common.enums.RoleType;
 import org.example.o_lim.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, RoleType> {}
--- src/main/java/org/example/o_lim/repository/TaskAssigneesRepository.java
+++ src/main/java/org/example/o_lim/repository/TaskAssigneesRepository.java
@@ -4,8 +4,10 @@ import org.example.o_lim.entity.TaskAssignees;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface TaskAssigneesRepository extends JpaRepository<TaskAssignees, Long> {
     @Query("""
         SELECT t FROM TaskAssignees t
